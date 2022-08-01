8c57ea64255d2bb158cd34497ab42e43bd19f08c
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2022-08-01 22:44:57 +0100

    Chapter 5: Implement Visitor Pattern

--- jlox/src/main/java/io/crocker/tool/GenerateAST.java
+++ jlox/src/main/java/io/crocker/tool/GenerateAST.java
@@ -2,6 +2,7 @@ package io.crocker.tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class GenerateAST {
 
     private static void defineAST(String outputDir, String baseName, List<String> types) throws IOException {
         String path = outputDir + "/" + baseName + ".java";
-        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
         writer.println("package io.crocker.jlox;");
         writer.println();
@@ -31,15 +32,30 @@ public class GenerateAST {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST Classes
         for (String type : types) {
             String className = type.split(":")[0].trim();
-            String fields  =type.split(":")[1].trim();
+            String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("       R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("    }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -55,6 +71,14 @@ public class GenerateAST {
             writer.println("            this." + name + " = " + name + ";");
         }
         writer.println("        }");
+
+        // Visitor pattern
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
 
         // Fields
         writer.println();
