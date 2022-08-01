46f19608f4336920655d7a46b5333875e04d2159
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2022-08-01 22:58:03 +0100

    Chapter 5: Add ASTPrinter

---
+++ jlox/src/main/test/io/crocker/jlox/ASTPrinterTest.java
@@ -0,0 +1,23 @@
+package io.crocker.jlox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ASTPrinterTest {
+
+    @Test
+    void print() {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)
+                )
+        );
+
+        assertEquals("(* (- 123) (group 45.67))", (new ASTPrinter()).print(expression));
+    }
+}
