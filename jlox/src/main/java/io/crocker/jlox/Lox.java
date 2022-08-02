8fa988ce4fe6d82e8ddf8d5c1f1f17a8a5f50dbf
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2022-08-02 18:48:52 +0100

    Chapter 7: Interpreter

--- jlox/src/main/java/io/crocker/jlox/Lox.java
+++ jlox/src/main/java/io/crocker/jlox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     /**
      * Entry-point to the Lox interpreter.
@@ -36,6 +39,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (Lox.hadError) System.exit(65);
+        if (Lox.hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -60,7 +64,7 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        System.out.println(new ASTPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -73,6 +77,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
