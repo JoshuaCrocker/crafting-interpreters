f2dbe91d4d6f50148e4596cd77cff272f3840a72
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2022-08-13 21:14:32 +0100

    Chapter 11: Fix Scoping Issue

--- jlox/src/main/java/io/crocker/jlox/Environment.java
+++ jlox/src/main/java/io/crocker/jlox/Environment.java
@@ -33,6 +33,10 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variables '" + name.lexeme +"'.");
     }
 
+    public void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     public Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -41,5 +45,17 @@ public class Environment {
         if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable  '" + name.lexeme + "'.");
+    }
+
+    public Object getAt(Integer distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    private Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
     }
 }
--- jlox/src/main/java/io/crocker/jlox/Lox.java
+++ jlox/src/main/java/io/crocker/jlox/Lox.java
@@ -70,6 +70,12 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        // Stop if there was an error in the resolver.
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
