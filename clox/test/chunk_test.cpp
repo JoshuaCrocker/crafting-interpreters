75eb0e9b71cfc9f159dfe6005263ff1ad10a6906
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2022-08-27 16:48:31 +0100

    Unit Test Chunk

---
+++ clox/test/chunk_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include "chunk.h"
+
+TEST(clox, initChunk) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    ASSERT_EQ(chunk.count, 0);
+    ASSERT_EQ(chunk.capacity, 0);
+    ASSERT_EQ(chunk.code, NULL);
+    ASSERT_EQ(chunk.lines, NULL);
+}
+
+TEST(clox, freeChunk) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    // Add data to chunk...
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // ...then free
+    freeChunk(&chunk);
+
+    ASSERT_EQ(chunk.count, 0);
+    ASSERT_EQ(chunk.capacity, 0);
+    ASSERT_EQ(chunk.code, NULL);
+    ASSERT_EQ(chunk.lines, NULL);
+}
+
+TEST(clox, writeChunk) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    // Add data to chunk...
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    ASSERT_EQ(chunk.count, 2);
+    ASSERT_EQ(chunk.capacity, 8);
+    // FIXME ASSERT_EQ(chunk.code, NULL);
+    // FIXME ASSERT_EQ(chunk.lines, NULL);
+}
